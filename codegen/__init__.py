e4162cc2a193c6138b063702687a590757aa84dc
ryt <tang_1995@126.com>
2021-08-17 03:33:31 +0800

    upt

--- codegen/__init__.py
+++ codegen/__init__.py
@@ -3,9 +3,9 @@ import logging
 from .log_utils import log_init
 from .op import zero_deps
 from .op import unary
+from .op import asserts
 from .op import power
 from .op import binary
-from .op import asserts
 from .op import cnds
 from .op import monomial
 from .op import polynomial

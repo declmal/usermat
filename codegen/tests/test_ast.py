f1286a13a374a367b539d9c210622ce69ec2f688
ryt <tang_1995@126.com>
2021-08-29 13:06:32 +0800

    upt

--- codegen/tests/test_ast.py
+++ codegen/tests/test_ast.py
@@ -2,6 +2,7 @@ import unittest
 
 from ..ast import codegen_line, TupleCommaList, Declaration
 from .test_utils import register_test
+from ..utils.type_utils import MinusOne, One, Half
 from ..op_def import OpDef as od
 from ..op_reg import OpReg as org
 
@@ -38,11 +39,39 @@ class TestAst(unittest.TestCase):
     def test_sin(self):
         v0 = od.var("x")
         v1 = od.sin(v0)
-        codegen_func = org.get_opt(v1, "dfs_ast")
+        ast_func = org.get_opt(v1, "dfs_ast")
         assignments = []
-        codegen_func(v1, [], [], assignments)
+        ast_func(v1, [], [], assignments)
         assignment = assignments[0]
         code = assignment.codegen()
         name = v1.name
         code_ref = "      {} = sin(x)".format(name)
         self.assertEqual(code, code_ref)
+
+    # def test_monomial(self):
+        # v0 = od.var("x")
+        # v1 = od.var("y")
+        # v2 = od.var("z")
+        # v3 = od.var("u")
+        # s0 = od.scalar(MinusOne)
+        # s1 = od.scalar(One)
+        # s2 = od.scalar(Half)
+        # s3 = od.scalar(-Half)
+        # op_list = [
+            # od.monomial(s0, v0, s0, v1, s1, v2, s2, v3, s3),
+            # od.monomial(s1, v0, s0, v1, s1, v2, s2, v3, s3),
+            # od.monomial(s2, v0, s0, v1, s1, v2, s2, v3, s3),
+            # od.monomial(s3, v0, s0, v1, s1, v2, s2, v3, s3)
+        # ]
+        # code_list = []
+        # for op in op_list:
+            # ast_func = org.get_opt(op, "dfs_ast")
+            # assignments = []
+            # ast_func(v1, [], [], assignments)
+            # assignments = assignments[0]
+            # code = assignment.codegen()
+            # code_list.append(code)
+        # name_list = [op.name for op in op_list]
+        # code_ref_list = []
+        # for name in name_list:
+            # code_ref = "      {} = 1.0"

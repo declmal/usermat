d04ad553f631c51915a74dbe72ff05d6c9f114fd
ryt <tang_1995@126.com>
2021-09-15 09:37:55 +0800

    upt

--- fcodegen/tests/bsim.py
+++ fcodegen/tests/bsim.py
@@ -4,7 +4,7 @@ from os import path
 import matplotlib.pyplot as plt
 import numpy as np
 
-from .test_utils import register_test, validate_od_func
+from .test_utils import register_test
 from ..op_def import OpDef as od
 from ..utils.type_utils import One, Zero, MinusOne
 from ..utils.math_utils import get_piecewise_linear_info_consec
@@ -30,6 +30,10 @@ def polygen(func, x0, x1, nsegs=default_nsegs):
     xs = [x0+inc*i for i in range(1, nsegs)]
     pointlist = [(x, func(x)) for x in xs]
     return pointlist
+
+def estimate_yielding_strain(fy, E):
+    eps_y = fy / E
+    return eps_y
 
 def bond_stress_ref(s2, tau_max, s_peak, sR):
     assert sR > 1.6 * s_peak
@@ -71,7 +75,7 @@ def friction_stress_ref(s2, tau_max, s_peak):
     return tau_f
 
 def bearing_stress(
-    s2, tau_max, s_peak, sR, nsegs1=default_nsegs, nsegs2=default_nsegs):
+    s2_op, tau_max, s_peak, sR, nsegs1=default_nsegs, nsegs2=default_nsegs):
     """
     """
     assert sR > 1.6 * s_peak
@@ -95,15 +99,15 @@ def bearing_stress(
     ]
     for i in range(len(points)):
         points[i] = (points[i][0], points[i][1]*tau_max)
-    scalar_datas = get_piecewise_linear_info_consec(points, 0, 0)
-    scalars = [od.scalar(scalar_data) for scalar_data in scalar_datas]
+    scalars = get_piecewise_linear_info_consec(points, 0, 0)
+    scalar_ops = [od.scalar(scalar) for scalar in scalars]
     # ret
-    ratio = od.divide(s2, od.scalar(s_peak))
-    tau_b = od.piecewiselinear(ratio, *scalars)
+    ratio = od.divide(s2_op, od.scalar(s_peak))
+    tau_b = od.piecewiselinear(ratio, *scalar_ops)
     return tau_b
 
 def friction_stress(
-    s2, tau_max, s_peak, nsegs1=default_nsegs, nsegs2=default_nsegs):
+    s2_op, tau_max, s_peak, nsegs1=default_nsegs, nsegs2=default_nsegs):
     """ The
     """
     # scalars
@@ -122,59 +126,45 @@ def friction_stress(
     ]
     for i in range(len(points)):
         points[i] = (points[i][0], points[i][1]*tau_max)
-    scalar_datas = get_piecewise_linear_info_consec(points, 0, 0)
-    scalars = [od.scalar(scalar_data) for scalar_data in scalar_datas]
+    scalars = get_piecewise_linear_info_consec(points, 0, 0)
+    scalar_ops = [od.scalar(scalar) for scalar in scalars]
     # ret
-    ratio = od.divide(s2, od.scalar(s_peak))
-    tau_f = od.piecewiselinear(ratio, *scalars)
+    ratio = od.divide(s2_op, od.scalar(s_peak))
+    tau_f = od.piecewiselinear(ratio, *scalar_ops)
     return tau_f
 
-@validate_od_func
-def spliting_reduction(s1, hR):
+def spliting_reduction(s1_op, hR):
     """
     """
-    points = [(0.5, 1), (1, 0)]
-    scalar_datas = get_piecewise_linear_info_consec(points, 0, 0)
-    scalars = [od.scalar(scalar_data) for scalar_data in scalar_datas]
-    ratio = od.divide(s1, hR)
-    rho_n = od.piecewiselinear(ratio, *scalars)
-    return rho_n
+    points = [(0.5*hR, 1), (hR, 0)]
+    scalars = get_piecewise_linear_info_consec(points, 0, 0)
+    scalar_ops = [od.scalar(scalar) for scalar in scalars]
+    rho_n_op = od.piecewiselinear(s1_op, *scalar_ops)
+    return rho_n_op
 
-@validate_od_func
+def yielding_reduction(eps_s_op, eps_y, eps_sh):
+    """
+    """
+    assert eps_y < eps_sh, "eps_y: {}, eps_sh: {}".format(eps_y, eps_sh)
+    points = [(eps_y, 1), (eps_sh, 0)]
+    scalars = get_piecewise_linear_info_consec(points, 0, 0)
+    scalar_ops = [od.scalar(scalar) for scalar in scalars]
+    rho_bs_op = od.piecewiselinear(eps_s_op, *scalar_ops)
+    return rho_bs_op
+
+def hardening_reduction(eps_s_op, eps_sh, eps_u):
+    """
+    """
+    assert eps_sh < eps_u, "eps_sh: {}, eps_u: {}".format(eps_sh, eps_u)
+    points = [(eps_sh, 1), (eps_u, 0)]
+    scalars = get_piecewise_linear_info_consec(points, 0, 0)
+    scalar_ops = [od.scalar(scalar) for scalar in scalars]
+    rho_fs_op = od.piecewiselinear(eps_s_op, *scalar_ops)
+    return rho_fs_op
+
 def bond_stress(
     tau_b, tau_f, rho_n, rho_bs, rho_fs, rho_bc, rho_fc):
-    """ The bond resistance considers both bearing and friction resistance,
-        intended for initial loading and loading beyond the maximum slip
-        values attained in the precious cycles.
-
-        Parameters
-        ----------
-        tau_b: fcodegen.op.base.Op
-            Full bearing stress of an elastic bar subject to a monotonic
-            pullout action.
-        tau_f: fcodegen.op.base.Op
-            Full friction stress of an elastic bar subject to a monotonic
-            pullout action.
-        rho_n: fcodegen.op.base.Op
-            A bond stress reduction factor that accounts for the opening of
-            splitting cracks in concrete.
-        rho_bs: fcodegen.op.base.Op
-            A bearing stress reduction factor that accounts for the yielding
-            of the bar in tension.
-        rho_fs: fcodegen.op.base.Op
-            A friction stress reduction factor that accounts for the yielding
-            of the bar in tension.
-        rho_bc: fcodegen.op.base.Op
-            A bearing stress reduction factor that accounts for the
-            loading-history-dependent bond deterioration.
-        rho_fc: fcodegen.op.base.Op
-            A friction stress reduction factor that accounts for the
-            loading-history-dependent bond deterioration.
-
-        Returns
-        -------
-        tau2: fcodegen.op.base.Op
-            Bond stress.
+    """
     """
     mono1 = od.monomial(one, rho_bs, one, rho_bc, one, tau_b, one)
     mono2 = od.monomial(one, rho_fs, one, rho_fc, one, tau_f, one)
@@ -182,28 +172,7 @@ def bond_stress(
     tau2 = od.monimial(one, rho_n, one, poly, one)
     return tau2
 
-@validate_od_func
 def normal_stress(tau2, theta, K_pen1, s1):
-    """ Normal stress is to simulate the wedging action of the bar ribs.
-
-        Parameters
-        ----------
-        tau2: fcodegen.op.base.Op
-            Bond stress.
-        theta: fcodegne.op.zero_deps.Scalar
-            A constant inclination angle with respect to the longitudinal axis
-            of the bar.
-        K_pen1: fcodegen.op.zero_deps.Scalar
-            A penalty stiffness introduced to minimize the interpenetration
-            of the steel and concrete.
-        s1: fcodegen.op.base.Op
-            Normal relative displacement.
-
-        Returns
-        -------
-        sig1: fcodegen.op.base.Op
-            Normal stress.
-    """
     op1 = od.abs(tau2)
     op2 = od.tan(theta)
     mono1 = od.monomial(minus_one, op1, one, op2, one)
@@ -230,10 +199,27 @@ def transverse_tangential_stress(K_pen3, s3):
     tau3 = od.monomial(one, K_pen3, one, s3, one)
     return tau3
 
+def plot_fig(*args, fname="fig.png", fdir=path.expanduser("~/Desktop")):
+    fpath = path.join(fdir, fname)
+    for i in range(0, len(args), 2):
+        x, y = args[i:i+2]
+        plt.plot(x, y)
+    fig = plt.gcf()
+    fig.savefig(fpath)
+    plt.close()
+
+def gen_data(func, low, high, num_points=1000):
+    assert low < high, "low: {}, high: {}".format(low, high)
+    xs = np.linspace(low, high, num_points)
+    ys = []
+    for x in xs:
+        y = func(x)
+        ys.append(y)
+    return xs, ys
 
 @register_test
 class TestBSIM(unittest.TestCase):
-    def test_stress(self):
+    def test_bond_stress(self):
         fcp = 40
         db = 18
         tau_max = estimate_peak_bond_stress(fcp)
@@ -241,28 +227,51 @@ class TestBSIM(unittest.TestCase):
         sR = estimate_clear_spacing(db)
         s2_op = od.var("var")
         tau_b_op = bearing_stress(s2_op, tau_max, s_peak, sR)
-        tau_f_op = friction_stress(s2_op, tau_max, s_peak)
+        g = Graph([s2_op], [tau_b_op])
 
-        g1 = Graph([s2_op], [tau_b_op, tau_f_op])
-        s2_lst = np.linspace(-2*sR, 2*sR, 1000)
-        tau_b_lst = []
-        tau_f_lst = []
-        tau_b_lst_ref = []
-        tau_f_lst_ref = []
-        for s2 in s2_lst:
-            tau_b, tau_f = g1.forward(s2)
-            tau_b_lst.append(tau_b)
-            tau_f_lst.append(tau_f)
-            tau_b_ref = bond_stress_ref(s2, tau_max, s_peak, sR)
-            tau_b_lst_ref.append(tau_b_ref)
-            tau_f_ref = friction_stress_ref(s2, tau_max, s_peak)
-            tau_f_lst_ref.append(tau_f_ref)
-        plt.subplot(2, 1, 1)
-        plt.plot(s2_lst, tau_b_lst, 'r')
-        plt.plot(s2_lst, tau_b_lst_ref, 'b')
-        plt.subplot(2 ,1, 2)
-        plt.plot(s2_lst, tau_f_lst, 'r')
-        plt.plot(s2_lst, tau_f_lst_ref, 'b')
-        fig = plt.gcf()
-        fig_path = path.expanduser("~/Desktop/TestBSIM.stress.png")
-        fig.savefig(fig_path)
+        s2_lst, tau_b_lst = gen_data(g.forward, -2*sR, 2*sR)
+        s2_lst_ref, tau_b_lst_ref = gen_data(
+            lambda x: bond_stress_ref(x, tau_max, s_peak, sR),
+            -2*sR, 2*sR)
+        plot_fig(
+            s2_lst, tau_b_lst, s2_lst_ref, tau_b_lst_ref,
+            fname="bond_stress.png")
+
+    def test_friction_stress(self):
+        fcp = 40
+        db = 18
+        tau_max = estimate_peak_bond_stress(fcp)
+        s_peak = estimate_peak_bond_stress_slip(db)
+        s2_op = od.var("var")
+        tau_f_op = friction_stress(s2_op, tau_max, s_peak)
+        g = Graph([s2_op], [tau_f_op])
+
+        s2_lst, tau_f_lst = gen_data(g.forward, -2*s_peak, 2*s_peak)
+        s2_lst_ref, tau_f_lst_ref = gen_data(
+            lambda x: friction_stress_ref(x, tau_max, s_peak),
+            -2*s_peak, 2*s_peak)
+        plot_fig(
+            s2_lst, tau_f_lst, s2_lst_ref, tau_f_lst_ref,
+            fname="friction_stress.png")
+
+    def test_yielding_reduction(self):
+        fy = 400
+        E = 206000
+        eps_y = estimate_yielding_strain(fy, E)
+        eps_sh = 0.01
+        eps_s_op = od.var("eps_s")
+        rho_bs_op = yielding_reduction(eps_s_op, eps_y, eps_sh)
+        g = Graph([eps_s_op], [rho_bs_op])
+
+        eps_s_lst, rho_bs_lst = gen_data(g.forward, 0, 2*eps_sh)
+        plot_fig(eps_s_lst, rho_bs_lst, fname="yielding_reduction.png")
+
+    def test_hardening_reduction(self):
+        eps_sh = 0.01
+        eps_u = 0.15
+        eps_s_op = od.var("eps_s")
+        rho_fs_op = hardening_reduction(eps_s_op, eps_sh, eps_u)
+        g = Graph([eps_s_op], [rho_fs_op])
+
+        eps_s_lst, rho_fs_lst = gen_data(g.forward, 0, 2*eps_u)
+        plot_fig(eps_s_lst, rho_fs_lst, fname="hardening_reduction.png")
